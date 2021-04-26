1c28e15f4f76b4f47f924f2983525a61be320d74
cjdvnd0206 <cjdvndaudnjf@naver.com>
2021-04-26 15:02:30 +0900

    Initial Commit

---
+++ AmplifyAuthTest/AmplifyAuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmplifyAuthTestApp.swift
+//  AmplifyAuthTest
+//
+//  Created by user on 2021/04/26.
+//
+
+import SwiftUI
+
+@main
+struct AmplifyAuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
